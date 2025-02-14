c20196ca988ca566c50da18a52c7a96470f83f3f
IkramHeraoua <heraoua.ikramm@gmail.com>
2025-02-14 16:13:21 +0000

    logo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            //$table->string('username')->unique();
+            //$table->string('firstname');
+            //$table->string('lastname');
+            $table->string('name')->nullable();
             $table->string('image')->nullable()->default(null);
-            $table->integer('phone')->unique();
+            $table->integer('phone')->unique()->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
